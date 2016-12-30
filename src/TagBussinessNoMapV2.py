a1f684ba0f25d611eecb2a01290c644537c02a7f
csrgxtu <1246506786@qq.com>
2016-12-30 09:05:28 +0800

    品牌表格命中了151019记录

--- src/TagBussinessNoMapV2.py
+++ src/TagBussinessNoMapV2.py
@@ -21,13 +21,20 @@ def main():
     conn_string = "host='127.0.0.1' dbname='jmtool20161229'"\
     + " user='postgres' password='postgres'"
     conn = psycopg2.connect(conn_string)
+    cpPropCursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    count = 0
 
     reader = unicode_csv_reader(open('../data/商场品牌表.csv'))
     for filed1, filed2, filed3, filed4, filed5 in reader:
         print filed1, filed2, filed3, filed4, filed5
+        cpPropSql = "select * from tbl_cp_prop where point_name like '%" + filed1 + "%'"
+        cpPropCursor.execute(cpPropSql)
+        for cpPropRow in cpPropCursor:
+            count = count + 1
 
 
-    print "Done"
+    print "Done", count
 
 
 if __name__ == "__main__":
