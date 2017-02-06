efc321c70800b68bfaaa51ece2474f2497095649
csrgxtu <1246506786@qq.com>
2017-02-06 11:15:52 +0800

    4s店，农历17年第一次提交代码

--- src/Tag4S.py
+++ src/Tag4S.py
@@ -72,45 +72,31 @@ def insertCPTag(conn, cursor, data):
 
 	return True
 
-# insert tbl_cp_tag_result
-# conn connection to the database
-# cursor a db table cursor
-# data tuple
-def insertCPTagResult(conn, cursor, data):
-	cpTagResSQL = "INSERT INTO tbl_cp_tag_result(ref_cp_code, "\
-		"ref_tag_type_code, tag_name, tag_value, ref_area_code) "\
-		"VALUES(%(ref_cp_code)s, %(ref_tag_type_code)s, %(tag_name)s, "\
-		"%(tag_value)s, %(ref_area_code)s)"
-
-	cursor.executemany(cpTagResSQL, data)
-	conn.commit()
-	# print len(data), cursor.statusmessage
-
-	return True
-
-def isInCpTagRelation(conn, cursor, ref_cp_code, tagId):
-    sql = "select * from tbl_cp_tag_relation where ref_cp_code = '" + ref_cp_code + "' and ref_tag_definition_id = " + str(tagId) + " limit 1"
-    cursor.execute(sql)
-    rtv = cursor.fetchone()
-
-    if rtv is not None:
-        return True
-    else:
+def isInLst(Lst, query):
+    if len(query) == 0:
         return False
 
-def isInLst(Lst, query):
     for l in Lst:
-        if query in l:
-            return (True, Lst[0])
+        if len(l) == 0:
+            continue
 
-    return (False, False)
+        if query in l:
+            return True
+
+    return False
 
 def isInLstReverse(Lst, query):
-    for l in Lst:
-        if l in query:
-            return (True, Lst[0])
+    if len(query) == 0:
+        return False
 
-    return (False, False)
+    for l in Lst:
+        if len(l) == 0:
+            continue
+
+        if l in query:
+            return True
+
+    return False
 
 def main():
     connStr = "host='127.0.0.1' dbname='jmtool20161229' user='postgres' password='postgres'"
@@ -128,10 +114,13 @@ def main():
     Brands = []
     with open('../data/4stags.csv') as F:
         for line in F:
-            Brands.append(line.strip())
+            tmpLst = line.strip().split(' ')
+            # 车类，车型
+            Brands.append([tmpLst[0], ' '.join(tmpLst[1:])])
+            # Brands.append(line.strip())
 
     # point_name
-    cpPropSql = "select cp.point_code, cp.ref_area_code, p.point_name from tbl_cp cp inner join tbl_cp_prop p on cp.point_code = p.ref_cp_code where p.ref_cptype_code like 'CP-4S-SHOWROOM%'"
+    cpPropSql = "select cp.point_code, cp.ref_area_code, p.point_name from tbl_cp cp inner join tbl_cp_prop p on cp.point_code = p.ref_cp_code inner join tbl_user_task_brief b on b.ref_area_code = cp.ref_area_code where p.ref_cptype_code like 'CP-4S-SHOWROOM%' and (b.status_result = 1045 or b.status_result = 106 or b.status_result = 107 or b.status_result = 1067)"
     cpPropCursor.execute(cpPropSql)
     for cpPropRow in cpPropCursor:
         if len(cpTagLst) >= 100:
@@ -141,77 +130,41 @@ def main():
 
         # 循环Brands，拿着point_name和其匹配
         for brand in Brands:
-            tmpLst = brand.split(' ')
-
-            rtvReverse = isInLstReverse(tmpLst, cpPropRow['point_name']) # point_name中包含品牌或者型号
-
-            if rtvReverse[0]:
+            if brand[1] in cpPropRow['point_name']: # point_name中包含车型的
+            # tmpLst = brand
+            #
+            # rtvReverse = isInLstReverse(tmpLst, cpPropRow['point_name']) # point_name中包含品牌或者型号
+            #
+            # if rtvReverse:
+                print brand[1], 'POINT_NAME', cpPropRow['point_name']
                 tmpDict = {
                     'ref_cp_code': cpPropRow['point_code'],
-                    'ref_tag_definition_id': getTagId(conn, tagDefCursor, brand),
+                    'ref_tag_definition_id': 1,
                     'ref_area_code': cpPropRow['ref_area_code'],
-                    'ref_brand_code': getBrandCode(conn, brandCursor, cpPropRow['point_name'], '4S'),
+                    'ref_brand_code': None,
                     'flag': '4SV1.1'
                 }
                 cpTagLst.append(tmpDict)
+                break
             else:
-                rtv = isInLst(tmpLst, cpPropRow['point_name']) # 品牌中包含point_name
-                if rtv[0]:
+                if cpPropRow['point_name'] in brand[1]:
+                # rtv = isInLst(tmpLst, cpPropRow['point_name']) # 品牌中包含point_name
+                # if rtv:
+                    print brand[1], 'POINT_NAME', cpPropRow['point_name']
                     tmpDict = {
                         'ref_cp_code': cpPropRow['point_code'],
-                        'ref_tag_definition_id': getTagId(conn, tagDefCursor, brand),
+                        'ref_tag_definition_id': 1,
                         'ref_area_code': cpPropRow['ref_area_code'],
-                        'ref_brand_code': getBrandCode(conn, brandCursor, cpPropRow['point_name'], '4S'),
+                        'ref_brand_code': None,
                         'flag': '4SV1.1'
                     }
                     cpTagLst.append(tmpDict)
+                    break
 
     if len(cpTagLst) > 0:
         insertCPTag(conn, cpTagCursor, tuple(cpTagLst))
         print "Inserted", len(cpTagLst)
         cpTagLst = []
-
-    # # prop_value, the folloing sql will be fuck slow, 商场在tbl_cp_exprop里面没有数据，所以下面的逻辑可以不执行
-    # cpExPropSql = "select cp.point_code, cp.ref_area_code, ex.prop_value from tbl_cp cp inner join tbl_cp_prop p on cp.point_code = p.ref_cp_code inner join tbl_cp_exprop ex on cp.point_code = ex.ref_cp_code where p.ref_cptype_code like 'CP-HOSPITALP-ROOM%' and cp.point_code not in (select ref_cp_code from tbl_cp_tag)"
-    # cpExPropCursor.execute(cpExPropSql)
-    # cpTagLst = []
-    # for cpExPropRow in cpExPropCursor:
-    #     if len(cpTagLst) >= 100:
-    #         insertCPTag(conn, cpTagCursor, tuple(cpTagLst))
-    #         print "Inserted", len(cpTagLst)
-    #         cpTagLst = []
-    #
-    #     # 循环TagDefs，拿着point_name和里面的descriptions匹配
-    #     for tagDef in TagDefs:
-    #         tmpLst = tagDef[2].split(',')
-    #
-    #         rtvReverse = isInLstReverse(tmpLst, cpExPropRow['prop_value']) # point_name中包含科室
-    #
-    #         if rtvReverse[0]:
-    #             tmpDict = {
-    #                 'ref_cp_code': cpExPropRow['point_code'],
-    #                 'ref_tag_definition_id': tagDef[0],
-    #                 'ref_area_code': cpExPropRow['ref_area_code'],
-    #                 'ref_brand_code': getBrandCode(conn, brandCursor, cpExPropRow['prop_value'], 'HOSPITAL'),
-    #                 'flag': 'HV1.2'
-    #             }
-    #             cpTagLst.append(tmpDict)
-    #         else:
-    #             rtv = isInLst(tmpLst, cpPropRow['point_name']) # 科室中包含point_name
-    #             if rtv[0]:
-    #                 tmpDict = {
-    #                     'ref_cp_code': cpExPropRow['point_code'],
-    #                     'ref_tag_definition_id': tagDef[0],
-    #                     'ref_area_code': cpExPropRow['ref_area_code'],
-    #                     'ref_brand_code': getBrandCode(conn, brandCursor, cpExPropRow['prop_value'], 'HOSPITAL'),
-    #                     'flag': 'HV1.2'
-    #                 }
-    #                 cpTagLst.append(tmpDict)
-    #
-    # if len(cpTagLst) > 0:
-    #     insertCPTag(conn, cpTagCursor, tuple(cpTagLst))
-    #     print "Inserted", len(cpTagLst)
-    #     cpTagLst = []
 
 if __name__ == "__main__":
 	main()
